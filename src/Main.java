fa946d43d18c916e3e0cc4eb4f56e903f74e756f
Vijay Singh <vsingh@ischool.berkeley.edu>
2022-08-08 22:14:44 -0700

    Code for Operators

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
-import javax.xml.transform.Result;
-import java.sql.SQLOutput;
-
 public class Main {
 
-    public static void main(String[]  args) {
-        int result = 1 + 2; // 1 + 2 = 3
-        System.out.println("1+2 = " + result);
-        int previousResult = result;
-        System.out.println("previousResult = " + previousResult);
-        result = result - 1;
-        System.out.println("3 - 1 = " + result);
-        System.out.println("previousResult = " + previousResult);
+    public static void Alien(String[] args) {
 
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien!");
+            System.out.println("And I am not scared of Aliens!");
 
+        }
     }
 }
-
