cee5912387cdf8fb560ab1d9a8a024609cd270b0
Vijay Singh <vsingh@ischool.berkeley.edu>
2022-08-12 22:35:08 -0700

    Code for Operators

---
+++ src/Main1.java
@@ -0,0 +1,15 @@
+public class Main1 {
+
+    public static void main(String[] args) {
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1+2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult);
+
+
+    }
+}
+
